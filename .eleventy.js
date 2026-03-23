4112abbaa129e99d27e09160113e4aca57848794
thrialectics <203729272+thrialectics@users.noreply.github.com>
2026-03-23 16:59:28 -0400

    Add project card styles, robots/llms.txt, update threadlinking post for v3

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/llms.txt");
 
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
